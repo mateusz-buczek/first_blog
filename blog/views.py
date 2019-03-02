edd93c62acd321b2914a32f01558297c84922731
mateusz-buczek <mateusz.buczek89@gmail.com>
2019-03-02 21:12:11 +0100

    Started playing with html and css. Laid foundations for future refinement

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
+
 
 def blog_index(request):
-    return render(request, 'blog/blog_index.html', {})
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(request, 'blog/blog_index.html', {'posts': posts})
