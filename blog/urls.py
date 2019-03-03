2fb92338f2fc3700535977e0f53cdf47f5334e17
mateusz-buczek <mateusz.buczek89@gmail.com>
2019-03-03 16:13:20 +0100

    adding/editing posts functionality added.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.blog_index, name='blog'),
+    path('post/<int:pk>/', views.post_details, name='post_details'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
